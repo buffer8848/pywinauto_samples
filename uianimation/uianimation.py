233f4914bfe1f1776f8a020128e31ed50cea231c
buffer8848 <179770346@qq.com>
2018-12-02 01:14:46 +0800

    update

--- uianimation/uianimation.py
+++ uianimation/uianimation.py
@@ -7,14 +7,26 @@
 
 #--------------------------------------------------------------------------------------------------
 from pywinauto.application import Application
+from pywinauto import timings
+
 # Run a target application
 app = Application().start("notepad.exe")
 
-app.Notepad.Edit.set_text("hello world.");
+app.Notepad.Edit.set_text("hello world.")
 
 app.Notepad.menu_select("文件(F)->保存(S)")
 
-dialog = app["另存为"]
-dialog.Edit.set_text("hello.txt");
-dialog.SaveButton.click();
+dlg_save = app["另存为"]
+dlg_save.Edit.set_text("hello.txt")
+dlg_save["保存(&S)"].click()
+
+#如果有此文件，弹出覆盖框，点击确定覆盖
+dlg_save_replace = app["确认另存为"]
+try:
+    dlg_save_replace.wait("exists enabled visible ready", timeout=1)
+    dlg_save_replace["是(&Y)"].click()
+except TimeoutError:
+    None
+
+app.Notepad.close()
 
