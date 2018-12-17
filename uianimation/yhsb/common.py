2bb3ab688ffc9b1cf653f306d548c5e4d50c1e9a
buffer8848 <179770346@qq.com>
2018-12-18 00:18:05 +0800

    add send email

--- uianimation/yhsb/common.py
+++ uianimation/yhsb/common.py
@@ -7,6 +7,9 @@
 
 #--------------------------------------------------------------------------------------------------
 from pywinauto.application import Application
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
 def restart_if_app_exist(exepath):
     try:
@@ -15,7 +18,7 @@ def restart_if_app_exist(exepath):
                 app.kill()
     except Exception:
         None
-
+		
 #根据基金列表进行排序后，计算出其相对于屏幕的位置，用于鼠标点击选择操作
 def get_position_of_jijin_list(list, base=[103, 385], step = 15):
     try:
@@ -33,4 +36,37 @@ def get_position_of_jijin_list(list, base=[103, 385], step = 15):
     except Exception:
         None
 
+#判断当前窗口句柄是否含有白名单字段
+def verify_control_exception(control, whitelist):
+    for index in whitelist:
+        try:
+            if (control[index].exist()):
+                return True
+        except Exception:
+            None
+    return False
 
+#对当前窗口进行截图
+def capture_current_screen():
+    None
+
+#发送邮件通知到指定的帐户
+def send_email_to_admin(message, sender, reciever):
+    # 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
+    message = MIMEText(message, 'plain', 'utf-8')
+    message['From'] = Header("自动化测试程序", 'utf-8')   # 发送者
+    message['To'] =  Header("自动化使用者", 'utf-8')        # 接收者
+    
+    subject = '自动化测试程序发生异常，请处理！'
+    message['Subject'] = Header(subject, 'utf-8')
+    
+    smtpObj = smtplib.SMTP('smtp.qq.com', 25)
+    smtpObj.login('179770346@qq.com','cbkjnrbsvahjcaee')
+    try:
+        smtpObj.sendmail(sender, reciever, message.as_string())
+    except smtplib.SMTPException:
+        print("Error: 无法发送邮件")
+    smtpObj.quit()
+
+if __name__ == "__main__":
+    send_email_to_admin("helloworld", "179770346@qq.com", "120315155@qq.com")
